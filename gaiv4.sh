c13c9165fce1add024472c1f9993038bc980bcd3
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-05-28 15:54:27 +0800

    Update gaiv4.sh

--- gaiv4.sh
+++ gaiv4.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 sed -i "s/^#\?precedence ::ffff:0:0/96  100/precedence ::ffff:0:0/96  100/g" /etc/gai.conf;
-sed -i "s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/gai.conf;
